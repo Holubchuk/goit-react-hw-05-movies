3567395a4e5fe99799df2fcb82a97f6dda07f284
Holubchuk <sashagolubchuk3@gmail.com>
2024-01-13 12:17:47 -0500

    feature/first request

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
+import { requestTrending } from 'services/api';
+
 const HomePage = () => {
+    const featchPhotosByQuery = async () => {
+        try {
+          const data = await requestTrending();
+          console.log(data);
+        } catch (e) {
+        }
+      };
+      featchPhotosByQuery();
   return (
-    <div>HomePage</div>
+    <div>HomePage
+    </div>
   )
 }
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,2 +1,11 @@
 import axios from "axios";
 
+export async function requestTrending() {
+    const BASE_URL = 'https://api.themoviedb.org/3/trending/movie/day';
+    const KEY = 'd425559355e0879fed139185dbbb845a';
+  
+  
+    const response = await axios.get(`${BASE_URL}? api-key= ${KEY}` );
+    console.log(response);
+    return response.data;
+  }
