71d5961985e7b7734bf53aa910316c10297610e1
Holubchuk <sashagolubchuk3@gmail.com>
2024-01-13 16:47:30 -0500

    feature/movieDetails/css

--- src/components/ModieDetails/MovieDetails.jsx
+++ src/components/ModieDetails/MovieDetails.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import css from './MovieDetails.module.css';
 
 export const MovieDetails = ({ movieDetails }) => {
   return (
-    <div>
-      <h3>Title: {movieDetails.title}</h3>
+    <div className={css.detailsContainer}>
       <img
-        width="300px"
+        
         src={`https://image.tmdb.org/t/p/original${movieDetails.backdrop_path}`}
         alt={movieDetails.title}
+        className={css.detailsImg}
       />
-      <p>User vote: {movieDetails.vote_average}</p>
-      <p>Overview</p>
-      <p>{movieDetails.overview}</p>
-      <p>Genres: {movieDetails.genres.map(genre => genre.name).join(', ')}</p>
+      <h3 className={css.detailsTitle}>
+        {movieDetails.title}({new Date(movieDetails.release_date).getFullYear()}
+        )
+      </h3>
+      <p className={css.detailsVote}>User vote: {movieDetails.vote_average}</p>
+      <p className={css.detailsOverview}>Overview</p>
+      <p className={css.detailsOverViewText}>{movieDetails.overview}</p>
+      <p className={css.detailsGenres}>Genres</p>
+      <p className={css.detailsGenresText}>{movieDetails.genres.map(genre => genre.name).join(', ')}</p>
     </div>
   );
 };
